3b1f5567ccb4abd7d8e007683082ba409ccd5e80
DmitryGhoncharko <70809609+DmitryGhoncharko@users.noreply.github.com>
2024-03-10 00:41:58 +0300

    project initialization

---
+++ src/main/java/by/ghoncharko/markovbot/MarkovBotApplication.java
@@ -0,0 +1,13 @@
+package by.ghoncharko.markovbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkovBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ghoncharko/markovbot/MarkovBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ghoncharko.markovbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkovBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
